3c1e85a57dd27d300d3aefdec26085a16784f6e3
Micah Wood <micah@wpscholar.com>
2019-05-18 13:58:24 -0400

    Create a plugin file.
    WordPress looks for a file in the root of the plugin directory that contains specific file headers. These headers define the plugin name, URI, description, version, author, etc.
    
    Adding this file with the appropriate headers is the minimum requirement to get a plugin to show in the WordPress plugin listing screen. You can now go into WordPress and activate the plugin.
    
    Note that the folder name and main plugin file have the same slug. While it is possible to have a file with a different slug, using the same slug is a common practice. The directory slug and text domain should always be the same.

---
+++ wp-recipe-tutorial.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Plugin Name:  WP Recipe Tutorial
+ * Plugin URI:   https://github.com/wpscholar/wp-recipe-tutorial
+ * Description:  A modern recipe plugin for WordPress that utilizes the block editor.
+ * Version:      1.0
+ * Author:       Micah Wood
+ * Author URI:   https://wpscholar.com
+ * License:      GPL2
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:  wp-recipe-tutorial
+ * Domain Path:  /languages
+ */
+ 
